8a23c324f337ea23146283b4bd663d990225f843
Claudinei Rodrigo Wickert <claudineiwickert@gmail.com>
2022-02-09 12:47:50 -0300

    grupo mestre adicionado a listaHelp.py

--- BOT/listaHelp.py
+++ BOT/listaHelp.py
@@ -1,7 +1,32 @@
 def listaHelp(clienteServerGroupsID, settings):
     try:
+            if str(settings["grupoMestre"]) in  clienteServerGroupsID:
+                return ("\n Voce pode usar:"
+                        "\n !shared level. Verificar level minimo e maximo para compartilhamento de exp"
+                        "\n !mp msg. Mandar um poke para todos os usuarios do TS3"
+                        "\n !mv canal. Mover todos os usuarios do TS3 para um canal"
+                        "\n !afk tempo. Altera o tempo de mover do AFK deve ser passado o tempo em minutos"
+                        "\n !mvch origem destino. Mover usuarios de um canal para outro"
+                        "\n !addfd Nome personagem. Adicionar personagem na lista amigos"
+                        "\n !rmfd Nome personagem. Remover personagem na lista amigos"
+                        "\n !addfdgui Nome da guilda. Adicionar guilda na lista amigos"
+                        "\n !rmfdgui Nome guilda. Remover guilda na lista amigos"
+                        "\n !addem Nome personagem. Adicionar personagem na lista inimigos"
+                        "\n !rmem Nome personagem. Remover personagem na lista inimigos"
+                        "\n !ltfd mostra lista de amigos"
+                        "\n !ltem mostra lista de inimigos"),(
+                        "\n !ltuser mostra lista de usuarios"
+                        "\n !addemgui Nome da guilda. Adicionar guilda na lista inimigos"
+                        "\n !rmemgui Nome guilda. Remover guilda na lista inimigos"
+                        "\n !adduser <NomeUsuario> <Main> adicionar usuario ao banco de dados"
+                        "\n !rmuser <Main> remover usuario ao banco de dados"
+                        "\n !addmaker <NomeMain> <NomeMaker> adicionar maker ao main no banco de dados"
+                        "\n !rmmmaker <NomeMain> <NomeMaker> remover maker do main no banco de dados"
+                        "\n !boss <mensagem> Mass Poke para usuarios do grupoBoss"
+                        "\n !sell <mensagem> Mass Poke para usuarios do grupoVendas"
+                        )
 
-            if str(settings["grupoEditor"]) in  clienteServerGroupsID:
+            elif str(settings["grupoEditor"]) in  clienteServerGroupsID:
                 return ("\n Voce pode usar:"
                         "\n !shared level. Verificar level minimo e maximo para compartilhamento de exp"
                         "\n !mp msg. Mandar um poke para todos os usuarios do TS3"
